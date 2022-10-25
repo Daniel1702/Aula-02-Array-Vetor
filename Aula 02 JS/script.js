0b0779b459ef708bfdf5e63cdf9903eeef465ad5
Daniel1702 <106408440+Daniel1702@users.noreply.github.com>
2022-10-25 20:35:09 -0300

    Add files via upload
    Atualizando Aula 02 JS / Array & Vetores

--- Aula 02 JS/script.js
+++ Aula 02 JS/script.js
@@ -42,3 +42,28 @@ console.log(array.indexOf(true));
 // slice - retorna parte de um array existente 
 let novoArray = array.slice(0, 2);
 console.log(novoArray);
+
+console.log('----------------------------------------------------')
+// o que são objetos 
+// como declara um objeto 
+let object = {propriedade1: 'valor1', propriedade2: 'valor2'};
+// deve ser declarado entre chaves {} 
+console.log(object);
+
+// assim como arrays as propriedades de objetos também podem guardar quaisquer valores
+let object2 = {
+    string: 'string',
+    number: 1,
+    boolean: true,
+    array: ['array'],
+    objectInterno: {objetoInterno: 'objetoInterno'}};
+
+//como acessar propriedades de objetos 
+//usando o ponto "."
+console.log(object2.objectInterno);
+
+// desestruturando objeto
+let string = object2.string;
+let number = object2.number; 
+let boolean = object2.boolean;
+console.log(string, number, boolean);
